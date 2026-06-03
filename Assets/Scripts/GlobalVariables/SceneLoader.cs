cbd678d286382206cef864b15e209e8684174d22
vero0996 <wab5119@gmail.com>
2026-06-02 21:05:10 -0600

    Cambie cosillas por ahi

--- Assets/Scripts/GlobalVariables/SceneLoader.cs
+++ Assets/Scripts/GlobalVariables/SceneLoader.cs
@@ -5,26 +5,31 @@ public class SceneLoader : MonoBehaviour
 {
     public void GoToGameplay()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("Gameplay");
     }
 
     public void GoToMenu()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenu");
     }
 
     public void GoToResults()
     {
-        SceneManager.LoadScene("Victory"); 
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Victory");
     }
 
     public void ExitGame()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("Splash");
     }
 
     public void GoToCredits()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("Credits");
     }
 }
